340538f3cd02dd2295cdb7dab8f57d3cb0224e07
AfanasevSergey <105625608+AfanasevS@users.noreply.github.com>
2022-10-23 20:38:23 +0800

    Update VariablesTheme.java

--- VariablesTheme.java
+++ VariablesTheme.java
@@ -19,7 +19,7 @@ public class VariablesTheme {
         System.out.println("versionOfProcessor = " + versionOfProcessor);
         System.out.println("active = " + active);
 
-        System.out.println("\n " + "2. Расчет стоимости товара со скидкой");
+        System.out.println("\n2. Расчет стоимости товара со скидкой");
         int pricePen = 100;
         int priceBook = 200;
         int discount = 11;
@@ -29,13 +29,13 @@ public class VariablesTheme {
         System.out.println("Общая сумма товаров со скидкой " +
                 (((pricePen + priceBook) * (100 - discount)) / 100));
 
-        System.out.println("\n " + "3. Вывод на консоль слова JAVA");
+        System.out.println("\n3. Вывод на консоль слова JAVA");
         System.out.println("   j    a  v     v  a    ");
         System.out.println("   j   a a  v   v  a a    ");
         System.out.println("j  j  aaaaa  V V  aaaaa  ");
         System.out.println(" jj  a     a  V  a     a ");
 
-        System.out.println("\n " + "4. Отображение min и max значений числовых типов данных");
+        System.out.println("\n4. Отображение min и max значений числовых типов данных");
         byte byteMax = 127;
         short shortMax = 32767;
         int intMax = 2_147_483_647;
@@ -54,7 +54,7 @@ public class VariablesTheme {
                 + ", значение после инкремента" + (++longMax)
                 + ", значение после декремента " + (--longMax));
 
-        System.out.println("\n " + "5. Перестановка значений переменных");
+        System.out.println("\n5. Перестановка значений переменных");
         int a = 2;
         int b = 5;
         int temp = a;
@@ -66,40 +66,40 @@ public class VariablesTheme {
         System.out.println("a = " + a + ", b = " + b);
 
         System.out.println("С помощью арифметических операций");
-        a = a + b;
+        a += b;
         b = a - b;
-        a = a - b;
+        a -= b;
         System.out.println("a = " + a + ", b = " + b);
 
         System.out.println("С помощью побитовой операции");
-        a = a ^ b;
+        a ^= b;
         b = a ^ b;
-        a = a ^ b;
+        a ^= b;
         System.out.println("a = " + a + ", b = " + b);
 
-        System.out.println("\n " + "6. Вывод символов и их кодов");
-        char codeChar1 = 35;
-        char codeChar2 = 38;
-        char codeChar3 = 64;
-        char codeChar4 = 94;
-        char codeChar5 = 95;
+        System.out.println("\n6. Вывод символов и их кодов");
+        int codeChar1 = 35;
+        int codeChar2 = 38;
+        int codeChar3 = 64;
+        int codeChar4 = 94;
+        int codeChar5 = 95;
 
-        System.out.println("Код символа 35 в системе ASCII-таблицы - " + (char) codeChar1 + ".");
-        System.out.println("Код символа 38 в системе ASCII-таблицы - " + (char) codeChar2 + ".");
-        System.out.println("Код символа 64 в системе ASCII-таблицы - " + (char) codeChar3 + ".");
-        System.out.println("Код символа 94 в системе ASCII-таблицы - " + (char) codeChar4 + ".");
-        System.out.println("Код символа 95 в системе ASCII-таблицы - " + (char) codeChar5 + ".");
+        System.out.println("Код символа " + (char) codeChar1 + " в системе ASCII-таблицы");
+        System.out.println("Код символа " + (char) codeChar2 + " в системе ASCII-таблицы");
+        System.out.println("Код символа " + (char) codeChar3 + " в системе ASCII-таблицы");
+        System.out.println("Код символа " + (char) codeChar4 + " в системе ASCII-таблицы");
+        System.out.println("Код символа " + (char) codeChar5 + " в системе ASCII-таблицы");
 
 
-        System.out.println("\n " + "7. Отображение количества сотен, десятков и единиц числа");
+        System.out.println("\n7. Отображение количества сотен, десятков и единиц числа");
         int number = 123;
         int hundred = number / 100;
-        int decade = (number / 10) % 10;
+        int dozen = (number / 10) % 10;
         int ones = number % 10;
         System.out.println("Число " + number + " состоит из " + hundred + " сотни "
-                + decade + " десятков" + " и " + ones + " единиц.");
+                + dozen + " десятков" + " и " + ones + " единиц.");
 
-        System.out.println("\n " + "8. Вывод на консоль ASCII-арт Дюка");
+        System.out.println("\n8. Вывод на консоль ASCII-арт Дюка");
         char slash = '\\';
         char backSlash = '/';
         char openBraket = '(';
@@ -111,21 +111,21 @@ public class VariablesTheme {
                 + slash + "  " + "\n" + backSlash + underline + underline + underline
                 + underline + backSlash + slash + underline + underline + slash);
 
-        System.out.println("\n " + "9. Произведение и сумма цифр числа");
+        System.out.println("\n9. Произведение и сумма цифр числа");
         number = 345;
         hundred = number / 100;
-        decade = (number % 100) / 10;
+        dozen = (number % 100) / 10;
         ones = number % 10;
-        int sumDigits = hundred + decade + ones;
-        int multDigits = hundred * decade * ones;
+        int sumDigits = hundred + dozen + ones;
+        int multDigits = hundred * dozen * ones;
         System.out.println("Сумма цифр числа " + number + " - " + sumDigits + " .");
         System.out.println("Произведение цифр числа " + number + " - " + multDigits + " .");
 
-        System.out.println("\n " + "10. Преобразование секунд");
+        System.out.println("\n10. Преобразование секунд");
         int sourceSeconds = 86399;
         int hours = sourceSeconds / 3600;
         int minutes = (sourceSeconds / 60) % 60;
         int seconds = sourceSeconds % 60;
-        System.out.println(hours + " : " + minutes + " : " + seconds);
+        System.out.println(hours + ":" + minutes + ":" +  seconds);
     }
 }
